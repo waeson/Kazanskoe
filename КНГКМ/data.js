d0976818fad901d85961731e312ebc1b3511deb6
waeson <59735306+waeson@users.noreply.github.com>
2025-09-17 00:32:10 +0700

    КНГКМ ВР

---
+++ КНГКМ/data.js
@@ -0,0 +1,847 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0---",
+      "name": "КНГКМ ВЖК обзорная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.982926357953941,
+        "pitch": 0.45954666064399596,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.730249559484694,
+          "pitch": 0.3914501559822252,
+          "rotation": 3.141592653589793,
+          "target": "1---1-"
+        },
+        {
+          "yaw": -2.907881401048762,
+          "pitch": 0.22945070692934344,
+          "rotation": 3.141592653589793,
+          "target": "13---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---1-",
+      "name": "КНГКМ коридор 1 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 1.5798142289549393,
+        "pitch": 0.2535226905718737,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.675796997553869,
+          "pitch": 0.07596640722393566,
+          "rotation": 4.71238898038469,
+          "target": "3--"
+        },
+        {
+          "yaw": 1.8027053174570966,
+          "pitch": 0.07746513349303541,
+          "rotation": 1.5707963267948966,
+          "target": "15--"
+        },
+        {
+          "yaw": 1.5572379198397126,
+          "pitch": 0.06461008506847676,
+          "rotation": 4.71238898038469,
+          "target": "12--"
+        },
+        {
+          "yaw": -3.0315862497086616,
+          "pitch": -0.08180615671280123,
+          "rotation": 5.497787143782138,
+          "target": "2---2-"
+        },
+        {
+          "yaw": 2.994728232784155,
+          "pitch": 0.2509776255218643,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2---2-",
+      "name": "КНГКМ коридор 2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.983798872469915,
+          "pitch": 0.21010469827998435,
+          "rotation": 3.141592653589793,
+          "target": "4----"
+        },
+        {
+          "yaw": -0.8921079528443663,
+          "pitch": 0.3186585105296178,
+          "rotation": 1.5707963267948966,
+          "target": "16--"
+        },
+        {
+          "yaw": -1.1436824201637918,
+          "pitch": -0.060797204096219915,
+          "rotation": 1.5707963267948966,
+          "target": "11---"
+        },
+        {
+          "yaw": -1.0604981412009185,
+          "pitch": 0.11993712553672964,
+          "rotation": 1.5707963267948966,
+          "target": "10---"
+        },
+        {
+          "yaw": 0.3971015809240317,
+          "pitch": -0.013894461263731728,
+          "rotation": 4.71238898038469,
+          "target": "9---"
+        },
+        {
+          "yaw": 1.5547880951258133,
+          "pitch": -0.017845824089821605,
+          "rotation": 3.141592653589793,
+          "target": "6---"
+        },
+        {
+          "yaw": -1.2327075749521725,
+          "pitch": 0.10109576831696643,
+          "rotation": 3.9269908169872414,
+          "target": "1---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "КНГКМ администратор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -1.1700289122179193,
+        "pitch": 0.2593887422537069,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0638890474587823,
+          "pitch": 0.04517767899421088,
+          "rotation": 1.5707963267948966,
+          "target": "1---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4----",
+      "name": "КНГКМ бильярд в коридоре",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6586275357597753,
+          "pitch": 0.04201625609845472,
+          "rotation": 3.141592653589793,
+          "target": "2---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--",
+      "name": "КНГКМ гладильная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.5808299861235948,
+        "pitch": 0.6628554132406812,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1204184388762135,
+          "pitch": 0.3266092214645493,
+          "rotation": 4.71238898038469,
+          "target": "12--"
+        },
+        {
+          "yaw": 2.138683910005737,
+          "pitch": 0.026268263954534632,
+          "rotation": 4.71238898038469,
+          "target": "1---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---",
+      "name": "КНГКМ гостевая комната",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.657037395807336,
+          "pitch": 0.12756359045874355,
+          "rotation": 9.42477796076938,
+          "target": "7---"
+        },
+        {
+          "yaw": 1.8599265152380688,
+          "pitch": 0.15307250587448706,
+          "rotation": 3.141592653589793,
+          "target": "8---"
+        },
+        {
+          "yaw": -1.0258756920339316,
+          "pitch": 0.02711124600792303,
+          "rotation": 1.5707963267948966,
+          "target": "2---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7---",
+      "name": "КНГКМ гостевая спальня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.6733335247910723,
+        "pitch": 0.3527678426914278,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8287212716285222,
+          "pitch": 0.015009407743985292,
+          "rotation": 3.141592653589793,
+          "target": "6---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8---",
+      "name": "КНГКМ гостевой санузел",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.005179619903426413,
+        "pitch": 0.1772584539381299,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0895161549583685,
+          "pitch": 0.21715672734097602,
+          "rotation": 3.141592653589793,
+          "target": "6---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9---",
+      "name": "КНГКМ комната отдыха",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.9574977198454135,
+        "pitch": 0.2283833297131661,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2870889893083213,
+          "pitch": -0.002791010602470223,
+          "rotation": 1.5707963267948966,
+          "target": "2---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10---",
+      "name": "КНГКМ компьютерный кабинет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.19486143967076686,
+        "pitch": 0.3904200468132153,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4349385339211462,
+          "pitch": 0.26642728358466883,
+          "rotation": 1.5707963267948966,
+          "target": "11---"
+        },
+        {
+          "yaw": 1.4049449303843247,
+          "pitch": -0.05529363888229888,
+          "rotation": 4.71238898038469,
+          "target": "2---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11---",
+      "name": "КНГКМ подсобное помещение",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.007646875414510035,
+          "pitch": 0.10664904028232058,
+          "rotation": 3.141592653589793,
+          "target": "1---1-"
+        },
+        {
+          "yaw": -0.022937051960520094,
+          "pitch": -0.129240815277889,
+          "rotation": 4.71238898038469,
+          "target": "2---2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12--",
+      "name": "КНГКМ прачечная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -2.019720197494035,
+        "pitch": 0.21279946206575318,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7240190952310037,
+          "pitch": 0.17105622384963226,
+          "rotation": 1.5707963267948966,
+          "target": "5--"
+        },
+        {
+          "yaw": 0.5853792415376766,
+          "pitch": -0.09100578738880927,
+          "rotation": 4.71238898038469,
+          "target": "1---1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13---",
+      "name": "КНГКМ спортзал большой",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.25519349403762,
+          "pitch": 0.05721758009732625,
+          "rotation": 0,
+          "target": "0---"
+        },
+        {
+          "yaw": -0.12783749636772868,
+          "pitch": -0.006099777170287979,
+          "rotation": 7.853981633974483,
+          "target": "14---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14---",
+      "name": "КНГКМ спортзал малый",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": 0.11790019522708661,
+        "pitch": 0.19184023086846658,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2910860034133762,
+          "pitch": 0.1862864451942361,
+          "rotation": 4.71238898038469,
+          "target": "13---"
+        },
+        {
+          "yaw": 1.3216593963586796,
+          "pitch": -0.07912821730356967,
+          "rotation": 5.497787143782138,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15--",
+      "name": "КНГКМ столовая",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "yaw": -0.8411550721403245,
+        "pitch": 0.3207731803653324,
+        "fov": 1.184663561926476
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.977663889326215,
+          "pitch": 0.0027669821621980617,
+          "rotation": 1.5707963267948966,
+          "target": "1---1-"
+        },
+        {
+          "yaw": 0.5722258698220273,
+          "pitch": -0.02194254053128475,
+          "rotation": 0,
+          "target": "0---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16--",
+      "name": "КНГКМ сушильная",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.78525060910739,
+          "pitch": -0.06122849997402646,
+          "rotation": 4.71238898038469,
+          "target": "2---2-"
+        },
+        {
+          "yaw": -2.5329328167422123,
+          "pitch": 0.3708954145683663,
+          "rotation": 1.5707963267948966,
+          "target": "10---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "КНГКМ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
